4dd4a2877d5b1accd04a8ad0547311ffdfd15075
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-29 17:04:57 -0300

    Added a div element on the App component that renders, for each country in flagData, its flag and name on the screen.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,23 @@
+import flagData from "../data/data.js";
+console.log(flagData);
+
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div className="App">
+      {flagData.map((country) => {
+        return (
+          <div key={country.id} id={country.id} className="card">
+            <img
+              id={country.id + "-img"}
+              src={country.flag}
+              alt={country.name + " Flag"}
+            />
+            <p id={country.id + "-text"}>{country.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
