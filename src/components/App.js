a77180be3cd70ca4c8410ab7485b80ef512dd2cc
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-29 18:15:34 -0300

    Added state variable called countryData that stores all countries object data in an array. Before storing, the array is shuffle so that it renders a different country order everytime the user opens the page.Added a function that shuffles the countries array and passed this function to the Card component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
+import React, { useState } from "react";
 import Cards from "./Cards.js";
+import shuffle from "../shuffle.js";
+import countries from "../data/data.js";
 
 function App() {
+  const [countryData, setCountryData] = useState(shuffle(countries));
+
+  function shuffleCountries() {
+    const shuffledCountryData = [...shuffle(countryData)];
+    setCountryData(shuffledCountryData);
+  }
+
   return (
     <div className="App">
-      <Cards />
+      <Cards countryData={countryData} shuffleCountries={shuffleCountries} />
     </div>
   );
 }
