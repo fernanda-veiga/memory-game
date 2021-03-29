360ee2d3479521cd5281f63d33a920c6c703e3b6
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-29 16:54:48 -0300

    Created an object to store all the flag data needed for the application. Imported flag images into the file. The object stores the country name, flag and the id of the element in the dom.

---
+++ src/data/data.js
@@ -0,0 +1,45 @@
+import flagAUS from "./flags/aus.png";
+import flagBEL from "./flags/bel.png";
+import flagBRA from "./flags/bra.png";
+import flagCAN from "./flags/can.png";
+import flagDEU from "./flags/deu.png";
+import flagGRC from "./flags/grc.png";
+import flagHRV from "./flags/hrv.png";
+import flagHUN from "./flags/hun.png";
+import flagIND from "./flags/ind.png";
+import flagISR from "./flags/isr.png";
+import flagITA from "./flags/ita.png";
+import flagJPN from "./flags/jpn.png";
+import flagKOR from "./flags/kor.png";
+import flagNLD from "./flags/nld.png";
+import flagPHL from "./flags/phl.png";
+import flagPRI from "./flags/pri.png";
+import flagSGP from "./flags/sgp.png";
+import flagSRB from "./flags/srb.png";
+import flagTUR from "./flags/tur.png";
+import flagUSA from "./flags/usa.png";
+
+const flagData = {
+  aus: { name: "Australia", flag: flagAUS, id: "aus" },
+  bel: { name: "Belgium", flag: flagBEL, id: "bel" },
+  bra: { name: "Brazil", flag: flagBRA, id: "bra" },
+  can: { name: "Canada", flag: flagCAN, id: "can" },
+  deu: { name: "Germany", flag: flagDEU, id: "deu" },
+  grc: { name: "Greece", flag: flagGRC, id: "grc" },
+  hrv: { name: "Croatia", flag: flagHRV, id: "hrv" },
+  hun: { name: "Hungary", flag: flagHUN, id: "hun" },
+  ind: { name: "India", flag: flagIND, id: "ind" },
+  isr: { name: "Israel", flag: flagISR, id: "isr" },
+  ita: { name: "Italy", flag: flagITA, id: "ita" },
+  jpn: { name: "Japan", flag: flagJPN, id: "jpn" },
+  kor: { name: "South Korea", flag: flagKOR, id: "kor" },
+  nld: { name: "Netherlands", flag: flagNLD, id: "nld" },
+  phl: { name: "Philippines", flag: flagPHL, id: "phl" },
+  pri: { name: "Puerto Rico", flag: flagPRI, id: "pri" },
+  sgp: { name: "Singapore", flag: flagSGP, id: "sgp" },
+  srb: { name: "Serbia", flag: flagSRB, id: "srb" },
+  tur: { name: "Turkey", flag: flagTUR, id: "tur" },
+  usa: { name: "United States", flag: flagUSA, id: "usa" },
+};
+
+export default flagData;
