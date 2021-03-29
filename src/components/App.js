b30237ecda888eee815aa65b777ad2ed52cb22b8
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-29 20:50:24 -0300

    Added a function that is called when the user clicks a card that increases the user's score by one and stores it in a state variable called currentScore. Sent currentScore as a props to the Header component.

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,11 @@ import countries from "../data/countries";
 function App() {
   const [countryData, setCountryData] = useState(shuffle(countries));
   const [clickedFlags, setClickedFlags] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
 
   function handleClick(event) {
     storeClickedFlag(event.target.id);
+    changeCurrentScore();
     shuffleCountries();
   }
 
@@ -21,6 +23,12 @@ function App() {
     console.log(clickedFlags);
   }
 
+  function changeCurrentScore() {
+    const newScore = currentScore + 1;
+    setCurrentScore(newScore);
+    console.log(currentScore);
+  }
+
   function shuffleCountries() {
     const shuffledCountryData = [...shuffle(countryData)];
     setCountryData(shuffledCountryData);
@@ -28,7 +36,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Header currentScore={currentScore} />
       <Cards countryData={countryData} handleClick={handleClick} />
       <Footer />
     </div>
