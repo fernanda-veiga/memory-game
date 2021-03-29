f3abd2e6e605b8be894239a8da542c24dac409fc
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-29 20:51:26 -0300

    The user's currentScore was passed as props and it is displayed in the scoreboard.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "../styles/Header.css";
 
-function Header() {
+function Header(props) {
   return (
     <header>
       <h1>Flags Memory Game</h1>
       <div className="score">
-        <p>SCORE: 0</p>
+        <p>SCORE: {props.currentScore}</p>
         <p>HIGH SCORE: 0</p>
       </div>
     </header>
