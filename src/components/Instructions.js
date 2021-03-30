54e5deb27acd6833d2a2933b765e6755235aa021
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-03-29 21:47:32 -0300

    Fixed styling and display of elements.

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -6,9 +6,8 @@ function Instructions() {
     <div className="instructions">
       <p>
         Click on a flag to get points, but don't click on the same flag more
-        than once.
+        than once. If you get 20 points, you win!
       </p>
-      <p>If you get 20 points, you win!</p>
     </div>
   );
 }
